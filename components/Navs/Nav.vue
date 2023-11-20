f910ad9784151c51fcdac3706b3cbdfab1e396d0
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-11-20 12:27:08 +0100

    wip

--- components/Navs/Nav.vue
+++ components/Navs/Nav.vue
@@ -3,15 +3,34 @@ const isOpen = ref(false);
 </script>
 
 <template>
-  <nav
-    class="flex items-center justify-between flex-wrap bg-pink-500 h-20 px-4 border-b border-pink-50"
-  >
-    <div>
-      <nuxt-link to="/">
-        <slot name="logo"> </slot>
-      </nuxt-link>
+  <nav>
+    <div
+      class="flex items-center justify-between flex-wrap bg-pink-500 h-20 px-4 border-b border-pink-50"
+    >
+      <div>
+        <nuxt-link to="/">
+          <slot name="logo"> </slot>
+        </nuxt-link>
+      </div>
+      <div>
+        <ul class="hidden md:flex items-center justify-between space-x-8">
+          <slot name="links"></slot>
+        </ul>
+        <ul>
+          <li class="flex md:hidden">
+            <button @click="isOpen = !isOpen">
+              <i v-if="!isOpen" class="ri-menu-line text-3xl"></i>
+              <div v-else>X</div>
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
-    <ul class="hidden md:flex flex items-center justify-between space-x-8">
+
+    <ul
+      v-show="isOpen"
+      class="md:hidden flex flex-col space-y-2 px-4 py-8 border-b border-white bg-gray-900"
+    >
       <slot name="links"></slot>
     </ul>
   </nav>
