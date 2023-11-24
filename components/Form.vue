407c6152f31db8b724c10310705cda2de1914031
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-11-24 12:44:30 +0100

    wip

--- components/Form.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <p class="text-pink-950 text-base font-serif">
-    <slot />
-  </p>
-</template>
--- pages/docs/composants/form.vue
+++ pages/docs/composants/form.vue
@@ -17,7 +17,8 @@ definePageMeta({
 
     <form>
       <label for="numberInput">Numéro : </label>
-      <input class="full-rounded"
+      <input
+        class="full-rounded"
         type="tel"
         pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
         id="numberInput"
@@ -26,12 +27,24 @@ definePageMeta({
       />
     </form>
 
-    <TextesCode code="<TitresH1>Titre de niveau 1</TitresH1>" />
+    <TextesCode
+      code='<form>
+      <label for="numberInput">Numéro : </label>
+      <input
+        class="full-rounded"
+        type="tel"
+        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        id="numberInput"
+        name="numberInput"
+        required
+      />
+    </form>'
+    />
 
     <TitresH2>Input email</TitresH2>
 
     <form>
-      <label for="emailInput">Email :</label>
+      <label for="emailInput">Email : </label>
       <input
         type="email"
         pattern=".+@example\.com"
@@ -41,21 +54,37 @@ definePageMeta({
       />
     </form>
 
-    <TextesCode code="<TitresH2>Titre de niveau 2</TitresH2>" />
+    <TextesCode
+      code='<form>
+      <label for="emailInput">Email :</label>
+      <input
+        type="email"
+        pattern=".+@example\.com"
+        id="emailInput"
+        name="emailInput"
+        required
+      />
+    </form>'
+    />
 
     <TitresH2>Checkbox</TitresH2>
 
     <form>
-      <label for="checkboxInput">Case à cocher :</label>
+      <label for="checkboxInput">Case à cocher : </label>
       <input type="checkbox" id="checkboxInput" name="checkboxInput" />
     </form>
 
-    <TextesCode code="<TitresH3>Titre de niveau 3</TitresH3>" />
+    <TextesCode
+      code='<form>
+      <label for="checkboxInput">Case à cocher : </label>
+      <input type="checkbox" id="checkboxInput" name="checkboxInput" />
+    </form>'
+    />
 
     <TitresH2>Select</TitresH2>
 
     <form>
-      <label for="selectInput">Sélectionner :</label>
+      <label for="selectInput">Sélectionner : </label>
       <select id="selectInput" name="selectInput">
         <option value="option1">Option 1</option>
         <option value="option2">Option 2</option>
@@ -63,20 +92,61 @@ definePageMeta({
       </select>
     </form>
 
-    <TextesCode code="<TitresH4>Titre de niveau 4</TitresH4>" />
+    <TextesCode
+      code='<form>
+      <label for="selectInput">Sélectionner : </label>
+      <select id="selectInput" name="selectInput">
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+    </form>'
+    />
 
     <TitresH2>Radio</TitresH2>
 
-    <label>Radio :</label>
-    <input type="radio" id="radioInput1" name="radioInput" value="radio1" />
-    <label for="radioInput1">Radio 1</label>
+    <form>
+      <label>Radio : </label>
+      <input
+        class="ml-3 mb-1"
+        type="radio"
+        id="radioInput1"
+        name="radioInput"
+        value="radio1"
+      />
+      <label for="radioInput1">Radio 1</label>
 
-    <input type="radio" id="radioInput2" name="radioInput" value="radio2" />
-    <label for="radioInput2">Radio 2</label>
+      <input
+        class="ml-3 mb-1"
+        type="radio"
+        id="radioInput2"
+        name="radioInput"
+        value="radio2"
+      />
+      <label for="radioInput2">Radio 2</label>
 
-    <input type="radio" id="radioInput3" name="radioInput" value="radio3" />
-    <label for="radioInput3">Radio 3</label>
+      <input
+        class="ml-3 mb-1"
+        type="radio"
+        id="radioInput3"
+        name="radioInput"
+        value="radio3"
+      />
+      <label for="radioInput3">Radio 3</label>
+    </form>
 
-    <TextesCode code="<TitresH4>Titre de niveau 4</TitresH4>" />
+    <TextesCode
+      code='<form>
+      <label>Radio : </label>
+      <input class="ml-3 mb-1" type="radio" id="radioInput1" name="radioInput" value="radio1" />
+      <label for="radioInput1">Radio 1</label>
+
+      <input class="ml-3 mb-1" type="radio" id="radioInput2" name="radioInput" value="radio2" />
+      <label for="radioInput2">Radio 2</label>
+
+      <input class="ml-3 mb-1" type="radio" id="radioInput3" name="radioInput" value="radio3" />
+      <label for="radioInput3">Radio 3</label>
+    </form>'
+    />
   </Container>
 </template>
