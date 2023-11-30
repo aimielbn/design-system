9e19b96ed84e82906aa683ca9fe1f894ec1a9943
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-11-30 15:10:17 +0100

    wip

--- pages/startup/equipe.vue
+++ pages/startup/equipe.vue
@@ -5,5 +5,47 @@ definePageMeta({
 </script>
 
 <template>
-    
+  <Container>
+    <TitresH1> Notre équipe </TitresH1>
+
+    <TextesParagraphe>
+      Voici notre équipe composée de 4 personnes ! Une équipe de qualité prête à
+      tout pour vous faire plaisir avec des produits faits main.
+    </TextesParagraphe>
+
+    <CardsUserCard>
+      <Image src="/img/3.jpg" alt="test" />
+      <TitresH2>Aimie, 20 ans</TitresH2>
+      <TextesParagraphe
+        >Aimie confectionne les bracelets brésiliens ! Une activité qui demande
+        du temps et de la patience.</TextesParagraphe
+      >
+    </CardsUserCard>
+
+    <CardsUserCard>
+      <Image src="/img/1.jpg" alt="test" />
+      <TitresH2>Aimie, 20 ans</TitresH2>
+      <TextesParagraphe
+        >Elle adore vous faire de petites bagues faisant ressortir votre
+        personnalité ! Des bagues uniques faites avec amour.</TextesParagraphe
+      >
+    </CardsUserCard>
+
+    <CardsUserCard>
+      <Image src="/img/2.jpg" alt="test" />
+      <TitresH2>Aimie, 20 ans</TitresH2>
+      <TextesParagraphe
+        >Cette femme incroyable s'occupe de faire des chaînes de téléphone
+        colorées et amusantes !</TextesParagraphe
+      >
+    </CardsUserCard>
+
+    <CardsUserCard>
+      <Image src="/img/4.jpg" alt="test" />
+      <TitresH2>Aimie, 20 ans</TitresH2>
+      <TextesParagraphe
+        >Et elle, s'occupe de la communication avec les clients.
+      </TextesParagraphe>
+    </CardsUserCard>
+  </Container>
 </template>
